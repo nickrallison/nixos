89edf96873534f1e89aef9ceebc1eaa9619b384b
nickrallison <nickrallison1@gmail.com>
2024-06-06 17:51:42 -0600

    Thu 06 Jun 2024 05:51:42 PM MDT

--- modules/home-manager/starship/starship.nix
+++ modules/home-manager/starship/starship.nix
@@ -3,11 +3,15 @@
     enable = true;
     # settings = pkgs.lib.importTOML ../starship.toml;
     settings = {
-      format = "[\\[$username\\]](yellow)[$directory](blue)$line_break$character";
+      format = "[\\[$custom.userid\\]](yellow)[$directory](blue)$line_break$character";
       time = {
         disabled = false;
         format = "🕙[\\[$time\\]]($style) ";
         time_format = "%H:%M:%S";
+      };
+      custom.userid = {
+        command = "echo $USER";
+        format = "[$output]($style)";
       };
     };
   };
