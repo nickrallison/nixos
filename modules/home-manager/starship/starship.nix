862caa1b5f39e765d99adbe9337d11015db1ec6a
nickrallison <nickrallison1@gmail.com>
2024-06-06 17:40:55 -0600

    Thu 06 Jun 2024 05:40:55 PM MDT

--- modules/home-manager/starship/starship.nix
+++ modules/home-manager/starship/starship.nix
@@ -1,6 +1,13 @@
 {pkgs, ...}: {
   programs.starship = {
     enable = true;
-    settings = pkgs.lib.importTOML ../starship.toml;
+    # settings = pkgs.lib.importTOML ../starship.toml;
+    settings = {
+      time = {
+        disabled = false;
+        format = "🕙[\\[$time\\]]($style) ";
+        time_format = "%H:%M:%S";
+      };
+    };
   };
 }
