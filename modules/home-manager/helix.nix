5a3fcd09a4cd90d07ebce87b9c6924b33e57eadf
nickrallison <nickrallison1@gmail.com>
2024-06-06 12:18:06 -0600

    11 current  2024-06-06 12:18:04  24.11.20240531.57610d2   6.6.32                          *

--- modules/home-manager/helix.nix
+++ modules/home-manager/helix.nix
@@ -2,23 +2,25 @@
 
 {
   programs.helix = {
-  enable = true;
-  settings = {
-    theme = "autumn_night_transparent";
-    editor.cursor-shape = {
-      normal = "block";
-      insert = "bar";
-      select = "underline";
+    enable = true;
+    settings = {
+      theme = "autumn_night_transparent";
+      editor = {
+        cursor-shape = {
+          normal = "block";
+          insert = "bar";
+          select = "underline";
+        };
+        whitespace = { render = "all"; };
+        true-color = true;
+      };
+    };
+    languages.language = [{ name = "nix"; }];
+    themes = {
+      autumn_night_transparent = {
+        "inherits" = "autumn_night";
+        "ui.background" = { };
+      };
     };
   };
-  languages.language = [{
-    name = "nix";
-  }];
-  themes = {
-    autumn_night_transparent = {
-      "inherits" = "autumn_night";
-      "ui.background" = { };
-    };
-  };
-};
 }
