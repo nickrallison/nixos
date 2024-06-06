450c21da9e6ad1253f13d615dcd73687766bf302
nickrallison <nickrallison1@gmail.com>
2024-06-06 12:32:43 -0600

    13 current  2024-06-06 12:32:40  24.11.20240531.57610d2   6.6.32                          *

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -100,10 +100,16 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    # Basic System Packages
+    vim 
     wget
     git
+
+    # Niceties
     ripgrep
+
+    # Dev
+    alejandra
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
