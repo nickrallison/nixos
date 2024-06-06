3256afbcfc1cd9647aa19e8f94f4ba683670c917
nickrallison <nickrallison1@gmail.com>
2024-06-06 16:52:50 -0600

    Thu 06 Jun 2024 04:52:50 PM MDT

--- modules/home-manager/starship.nix
+++ modules/home-manager/starship.nix
@@ -1,14 +1,5 @@
 {
   programs.starship = {
     enable = true;
-    settings = {
-      # "$schema" = "https://starship.rs/config-schema.json";
-      add_newline = true;
-      command_timeout = 500;
-      continuation_prompt = "[∙](bright-black) ";
-      format = "\[$username\]$hostname$directory$line_break$character";
-      right_format = "";
-      scan_timeout = 30;
-    };
   };
 }
