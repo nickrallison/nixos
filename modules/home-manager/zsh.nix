8e874c4c5e94764f491628e1e6071bde71ee8662
nickrallison <nickrallison1@gmail.com>
2024-06-06 15:19:47 -0600

    Thu 06 Jun 2024 03:19:47 PM MDT

---
+++ modules/home-manager/zsh.nix
@@ -0,0 +1,21 @@
+programs.zsh = {
+  enable = true;
+  enableCompletion = true;
+  autosuggestion.enable = true;
+  syntaxHighlighting.enable = true;
+
+  shellAliases = {
+    ll = "ls -l";
+    update = "sudo nixos-rebuild switch";
+  };
+  history = {
+    size = 10000;
+    path = "${config.xdg.dataHome}/zsh/history";
+  };
+  oh-my-zsh = {
+    enable = true;
+    plugins = [ "git" "thefuck" ];
+    theme = "robbyrussell";
+  };
+
+};
