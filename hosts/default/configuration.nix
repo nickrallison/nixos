cc347754332294007b41f676cb2225d1325a2890
nickrallison <nickrallison1@gmail.com>
2024-06-07 13:13:53 -0600

    Fri 07 Jun 2024 01:13:53 PM MDT

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -107,6 +107,9 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
+  # Enable tailscale
+  services.tailscale.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
