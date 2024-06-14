9779e29d0c8699d02cb4cf3d7e3640328778071b
nickrallison <nickrallison1@gmail.com>
2024-06-14 14:46:56 -0600

    Fri 14 Jun 2024 02:46:56 PM MDT

--- modules/home-manager/firefox.nix
+++ modules/home-manager/firefox.nix
@@ -5,83 +5,59 @@
   lib,
   pkgs,
   ...
-}:
-with lib;
-with lib.campground; let
-  cfg = config.campground.apps.firefox;
-in {
-  options.campground.apps.firefox = with types; {
-    enable = mkBoolOpt false "Whether or not to enable Firefox.";
-    cac = mkBoolOpt false "Enable CAC Support";
-  };
-
-  config = mkIf cfg.enable {
-    # environment.systemPackages = with pkgs; [
-    #   nssTools
-    #   firefox
-    # ];
-
-    programs.firefox = {
-      enable = true;
-      profiles = {
-        default = {
-          id = 0;
-          name = "default";
-          isDefault = true;
-          settings = {
-            "browser.startup.homepage" = "about:config";
-            "browser.search.defaultenginename" = "DuckDuckGo";
-            "browser.search.order.1" = "DuckDuckGo";
-          };
-          search = {
-            force = true;
-            default = "DuckDuckGo";
-            order = ["DuckDuckGo" "Google"];
-            engines = {
-              "Nix Packages" = {
-                urls = [
-                  {
-                    template = "https://search.nixos.org/packages";
-                    params = [
-                      {
-                        name = "type";
-                        value = "packages";
-                      }
-                      {
-                        name = "query";
-                        value = "{searchTerms}";
-                      }
-                    ];
-                  }
-                ];
-                icon = "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-                definedAliases = ["@np"];
-              };
-              "NixOS Wiki" = {
-                urls = [{template = "https://nixos.wiki/index.php?search={searchTerms}";}];
-                iconUpdateURL = "https://nixos.wiki/favicon.png";
-                updateInterval = 24 * 60 * 60 * 1000; # every day
-                definedAliases = ["@nw"];
-              };
-              "Searx" = {
-                urls = [{template = "https://searx.aicampground.com/?q={searchTerms}";}];
-                iconUpdateURL = "https://nixos.wiki/favicon.png";
-                updateInterval = 24 * 60 * 60 * 1000; # every day
-                definedAliases = ["@searx"];
-              };
-              "Bing".metaData.hidden = true;
-              "Google".metaData.alias = "@g"; # builtin engines only support specifying one additional alias
-            };
-          };
-          extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-            ublock-origin
-            bitwarden
-            darkreader
-          ];
+}: {
+  programs.firefox = {
+    enable = true;
+    profiles = {
+      default = {
+        id = 0;
+        name = "default";
+        isDefault = true;
+        settings = {
+          "browser.startup.homepage" = "about:config";
+          "browser.search.defaultenginename" = "DuckDuckGo";
+          "browser.search.order.1" = "DuckDuckGo";
         };
+        search = {
+          force = true;
+          default = "DuckDuckGo";
+          order = ["DuckDuckGo" "Google"];
+          engines = {
+            "Nix Packages" = {
+              urls = [
+                {
+                  template = "https://search.nixos.org/packages";
+                  params = [
+                    {
+                      name = "type";
+                      value = "packages";
+                    }
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+              icon = "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = ["@np"];
+            };
+            "NixOS Wiki" = {
+              urls = [{template = "https://nixos.wiki/index.php?search={searchTerms}";}];
+              iconUpdateURL = "https://nixos.wiki/favicon.png";
+              updateInterval = 24 * 60 * 60 * 1000; # every day
+              definedAliases = ["@nw"];
+            };
+            "Bing".metaData.hidden = true;
+            "Google".metaData.alias = "@g"; # builtin engines only support specifying one additional alias
+          };
+        };
+        extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+          ublock-origin
+          bitwarden
+          darkreader
+        ];
       };
     };
-    # # TODO: Add things to exploade cac certs and install them into firefox here
-    # campground.services.cac.enable = mkIf cfg.cac true;
   };
 }
