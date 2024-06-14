c6f51d09c304fe666ad21957dcd4fe2cadfa717c
nickrallison <nickrallison1@gmail.com>
2024-06-14 14:49:54 -0600

    Fri 14 Jun 2024 02:49:54 PM MDT

--- modules/home-manager/firefox.nix
+++ modules/home-manager/firefox.nix
@@ -1,63 +1,15 @@
 # firefox.nix
 {
-  options,
-  config,
-  lib,
-  pkgs,
-  ...
-}: {
   programs.firefox = {
     enable = true;
-    profiles = {
-      default = {
-        id = 0;
-        name = "default";
-        isDefault = true;
-        settings = {
-          "browser.startup.homepage" = "about:config";
-          "browser.search.defaultenginename" = "DuckDuckGo";
-          "browser.search.order.1" = "DuckDuckGo";
-        };
-        search = {
-          force = true;
-          default = "DuckDuckGo";
-          order = ["DuckDuckGo" "Google"];
-          engines = {
-            "Nix Packages" = {
-              urls = [
-                {
-                  template = "https://search.nixos.org/packages";
-                  params = [
-                    {
-                      name = "type";
-                      value = "packages";
-                    }
-                    {
-                      name = "query";
-                      value = "{searchTerms}";
-                    }
-                  ];
-                }
-              ];
-              icon = "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-              definedAliases = ["@np"];
-            };
-            "NixOS Wiki" = {
-              urls = [{template = "https://nixos.wiki/index.php?search={searchTerms}";}];
-              iconUpdateURL = "https://nixos.wiki/favicon.png";
-              updateInterval = 24 * 60 * 60 * 1000; # every day
-              definedAliases = ["@nw"];
-            };
-            "Bing".metaData.hidden = true;
-            "Google".metaData.alias = "@g"; # builtin engines only support specifying one additional alias
-          };
-        };
-        # extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-        #   ublock-origin
-        #   bitwarden
-        #   darkreader
-        # ];
-      };
+    arkenfox = {
+      enable = true;
+      version = "103.0";
+    };
+
+    profiles.Default.arkenfox = {
+      enable = true;
+      "0000".enable = true;
     };
   };
 }
